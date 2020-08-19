2a9c33014263e29de658661271f14d680fc4ef40
Seanny Phoenix <seannyphoenix@gmail.com>
2020-08-18 21:49:26 -0700

    version 1.0.0

--- react-rollup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-npm init -y
-npm install react react-dom
-npm install --save-dev rollup @rollup/plugin-babel eslint
-npx eslint --init
--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,36 @@
+import babel from '@rollup/plugin-babel';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import cleanup from 'rollup-plugin-cleanup';
+import minify from 'rollup-plugin-babel-minify';
+import copy from 'rollup-plugin-copy';
+import replace from '@rollup/plugin-replace';
+
+const copyFiles = [
+  { src: 'public/*', dest: 'build/' },
+];
+
+const rollupPlugins = [
+  babel({ babelHelpers: 'bundled' }),
+  nodeResolve({
+    extensions: ['.mjs', '.js', '.json', '.node', '.jsx'],
+  }),
+  replace({
+    'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+  }),
+  commonjs(),
+  copy({ targets: copyFiles }),
+];
+
+if (process.env.NODE_ENV === 'production') {
+  rollupPlugins.push(minify(), cleanup());
+}
+
 export default {
-  input: 'src/index.js',
+  input: 'src/index.jsx',
   output: {
     file: 'build/index.js',
     format: 'esm',
   },
+  plugins: rollupPlugins,
 };
